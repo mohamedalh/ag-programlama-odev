c822ee95876c5131519897e9b87b8f05c33f7992
mohamedalh <mooh1551995@gmail.com>
2024-12-19 19:52:49 +0300

    changed

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -61,7 +61,7 @@ export const useAuthStore = create((set, get) => ({
         try {
             await axiosInstance.post("/auth/logout");
             set({ authUser: null });
-            toast.success("Logged out successfully");
+            toast.success("aşarıyla çıkış yapıldı");
             get().disconnectSocket();
         } catch (error) {
             toast.error(error.response.data.message);
